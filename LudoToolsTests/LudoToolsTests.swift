8e0e9b91e457a1231cc819006d53f633d8c13f10
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2025-08-10 20:42:40 -0300

    Initial Commit

---
+++ LudoToolsTests/LudoToolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LudoToolsTests.swift
+//  LudoToolsTests
+//
+//  Created by Lucas Calheiros on 10/08/25.
+//
+
+import Testing
+@testable import LudoTools
+
+struct LudoToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
